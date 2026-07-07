0e82a59a3045a38f2c1b45210f5c6cf348e1eb5b
Devanshu Kumar <thepixelpoet1@gmail.com>
2026-07-07 09:26:57 +0530

    parsing the request headers and putting it in hashmap

--- source/httpServer.java
+++ source/httpServer.java
@@ -1,10 +1,12 @@
 package source;
 import java.io.*;
 import java.net.*;
+import java.util.HashMap;
 
 public class httpServer {
     public static void main (String [] args)throws IOException{
         int port = 8080;
+        HashMap<String, String> headers  = new HashMap<>();
         ServerSocket server = new ServerSocket(port);
         System.out.println("Serving at port:"+ port);
 
@@ -14,25 +16,35 @@ public class httpServer {
         BufferedReader reader = new BufferedReader(new InputStreamReader(client.getInputStream()));
 
         String line;
+        //The lines below are for taking the values in strings 
+        //and then printing them on the console.
         String requestLine = reader.readLine();
         String [] parts = requestLine.split(" ");
         String method = parts[0];
-        String path = parts[1];
+       String path = parts[1];
         String version = parts[2];
-
-
         System.out.println("Method: " + method);
         System.out.println("Path" + path);
         System.out.println("Version: " + version);
 
-        while(true){
-            if((line = reader.readLine())!=null){
 
-                if(line.isEmpty())break;
+        //for storing these headers in hashmaps
+        while(!(line = reader.readLine()).isEmpty()){
+           // System.out.println(line);
+            int seperator = line.indexOf(":");
+           // System.out.println(seperator);
+            String key = line.substring(0, seperator).trim();
+            String value = line.substring(seperator + 1).trim();
 
-                System.out.println(line);
-            }
+            headers.put(key, value);
         }
+
+
+        System.out.println("Host - " + headers.get("Host"));
+        System.out.println("User-Agent - " + headers.get("User-Agent"));
+        System.out.println("Connection - " + headers.get("Connection"));
+
+
 
         PrintWriter writer = new PrintWriter(client.getOutputStream());
         writer.print("HTTP/1.1 200 OK\r\n");
